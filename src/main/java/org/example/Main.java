66d0d2175333e7bcd56e939ed070677c7a12c65e
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-07-10 00:46:05 +0400

    conflict from master branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("jgvjhk");
+        System.out.println("original");
         System.out.println("vgb");
     }
 }
