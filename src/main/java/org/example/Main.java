320dc41ef70b4af2b8f13677fcb94e51f820289d
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-07-10 00:37:49 +0400

    conflict producing commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("jgvjhk");
-        System.out.println("vgb");
+        System.out.println("conflict accured");
     }
 }
